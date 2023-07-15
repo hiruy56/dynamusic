d3d990d00fe9d27a1828a649ee772ba96e870c43
hiruy56 <75577876+hiruy56@users.noreply.github.com>
2023-07-15 14:41:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
-from pytube import YouTube
+import youtube_dl
 from fastapi import FastAPI
 
 app = FastAPI()
-@app.get("/")
-def home():
-    return {"message": "home"}
 
-@app.get("/audio_stream")
-def get_audio_stream_url(url: str):
-    try:
-        # Get the first available audio stream URL
-        audio_url = YouTube(url).streams.filter(only_audio=True).first().url
-        return {"audio_stream_url": audio_url}
-    except Exception as e:
-        return {"error": str(e)}
+@app.get("/")
+def root():
+    return {"message": "Welcome to DynamusicAPI!"}
+
+@app.get("/extract-audio")
+def extract_audio(url: str):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'proxy': 'socks4://186.251.255.73:31337'
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        formats = info.get('formats', [])
+        for fmt in formats:
+            if fmt.get('acodec') == 'mp4a.40.2':
+                audio_url = fmt.get('url')
+                break
+
+    return {"audio_url": audio_url}
 
 if __name__ == "__main__":
     import uvicorn
