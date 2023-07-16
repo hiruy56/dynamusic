00a0f5f1d9ea834c7dc6e41f57c12c13fd67afc5
HIRUY METSIHAFE\DIO <75577876+hiruy56@users.noreply.github.com>
2023-07-16 08:47:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -35,6 +35,3 @@ def extract_audio(url: str):
     return {"audio_url": audio_url, "response_time": response_time}
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="localhost", port=8000)
